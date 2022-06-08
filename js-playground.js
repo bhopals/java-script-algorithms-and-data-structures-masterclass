afedb56300fa0ae1b9cd068b92db100f536a557c
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-06-08 15:47:22 -0400

    Problem Solving Patterns - Frequency Counter Pattern - Anagram Solution

--- js-playground.js
+++ js-playground.js
@@ -1,30 +1,32 @@
 //SAME
 
-function same(arr1, arr2) {
-  if (arr1.length != arr2.length) {
+function validAnagram(string1, string2) {
+  if (string1.length !== string2.length) {
     return false;
   }
-  let frequencyCounter1 = {};
-  let frequencyCounter2 = {};
-  for (let val of arr1) {
-    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
-  }
-  for (let val of arr2) {
-    frequencyCounter2[val] = (frequencyCounter1[val] || 0) + 1;
+  let frequencyCounterString1 = {};
+  let frequencyCounterString2 = {};
+
+  for (let str of string1.split("")) {
+    frequencyCounterString1[str] = (frequencyCounterString1[str] || 0) + 1;
   }
 
-  console.log("frequencyCounter1:", frequencyCounter1); // {1:1, 2:2, 3:1, 5:1}
-  console.log("frequencyCounter2:", frequencyCounter2); // {1:1, 4:2, 9:1, 11:1}
-  for (let key in frequencyCounter1) {
-    // If there is any KEY in frequencyCounter2 with KEY*2
-    if (!(key * 2 in frequencyCounter2)) {
-      return false;
-    }
-    // If there is any value in frequencyCounter matches with frequencyCounter2
-    if (frequencyCounter2[key * 2] !== frequencyCounter1[key]) {
-      return false;
-    }
-    return true;
+  for (let str of string2.split("")) {
+    frequencyCounterString2[str] = (frequencyCounterString2[str] || 0) + 1;
   }
+  console.log("frequencyCounterString1>", frequencyCounterString1);
+  console.log("frequencyCounterString2>", frequencyCounterString2);
+  for (let key in frequencyCounterString1) {
+    if (frequencyCounterString1[key] !== frequencyCounterString2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
-same([1, 2, 3, 4, 5], [9, 1, 4, 4, 11]);
+console.log(validAnagram("", ""), true); // true
+console.log(validAnagram("aaz", "zza"), false); // false
+console.log(validAnagram("anagram", "nagaram"), true); // true
+console.log(validAnagram("rat", "car"), false); // false
+console.log(validAnagram("awesome", "awesom"), false); // false
+console.log(validAnagram("qwerty", "qeywrt"), true); // true
+console.log(validAnagram("texttwisttime", "timetwisttext"), true); // true
