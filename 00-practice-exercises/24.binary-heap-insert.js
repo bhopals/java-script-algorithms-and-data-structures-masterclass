8cfaf22a688b803cb194fb91ed77774c6c4f83b7
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-06-21 09:11:07 -0400

    Binary Heaps: MAx Binary Heap Insert Psuedocode

---
+++ 00-practice-exercises/24.binary-heap-insert.js
@@ -0,0 +1,31 @@
+const maxBinaryHeap = [41, 39, 33, 18, 27, 12];
+
+const index = maxBinaryHeap.length - 1;
+
+class MaxBinaryHeap {
+  constructor(values) {
+    this.values = values || [];
+  }
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      const pIndex = Math.floor((idx - 1) / 2);
+      const pValue = this.values[pIndex];
+      if (element <= pValue) break;
+      this.values[pIndex] = element;
+      this.values[idx] = pValue;
+      idx = pIndex;
+    }
+  }
+}
+
+const binaryHeap = new MaxBinaryHeap(maxBinaryHeap);
+console.log(binaryHeap);
+binaryHeap.insert(55);
+console.log(binaryHeap);
