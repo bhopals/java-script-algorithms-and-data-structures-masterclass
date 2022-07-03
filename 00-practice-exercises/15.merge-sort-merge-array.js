31c44ffbbb253e29561a542d58e895165db0c19e
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-03 14:20:11 -0400

    Merge Sort: Merge Array - Tutor Solution

--- 00-practice-exercises/15.merge-sort-merge-array.js
+++ 00-practice-exercises/15.merge-sort-merge-array.js
@@ -1,3 +1,29 @@
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0,
+    j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+}
+
 function mergeArrays(arr1, arr2) {
   let merged = [];
   while (arr1.length > 0 && arr2.length > 0)
@@ -6,8 +32,8 @@ function mergeArrays(arr1, arr2) {
   return merged;
 }
 
-const a1 = [1, 10, 50];
-const a2 = [2, 14, 99, 100, 200, 300, 400];
+const mergedAr = mergeArrays([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
+console.log(mergedAr);
 
-const merged = mergeArrays(a1, a2); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
+const merged = merge([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
 console.log(merged);
