91c1209da4c8fcad2866d22e2b80b3ab12fab560
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-03 14:23:19 -0400

    Merge Sort: Merge Array Code Examples

--- 00-practice-exercises/15.merge-sort-merge-array.js
+++ 00-practice-exercises/15.merge-sort-merge-array.js
@@ -32,8 +32,8 @@ function mergeArrays(arr1, arr2) {
   return merged;
 }
 
-const mergedAr = mergeArrays([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
-console.log(mergedAr);
+const mergedAr = mergeArrays([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]);
+console.log(mergedAr); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
 
-const merged = merge([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
-console.log(merged);
+const merged = merge([1, 10, 50], [2, 14, 99, 100, 200, 300, 400]);
+console.log(merged); // [1, 2, 10, 14, 50, 99, 100, 200, 300, 400]
